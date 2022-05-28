2de2679c59771aa997cee1f693cdcba0d790d12b
Nisa <n.nurb@hotmail.com>
2022-05-28 23:44:11 +0300

    static files uploaded

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+//middleware
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sever is running on port: ${port}`);
+});
