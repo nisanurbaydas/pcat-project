8a12cc073cc57caadcc91497f04fd0571db03f8c
Nisa <n.nurb@hotmail.com>
2022-05-29 12:45:25 +0300

    template engine

--- app.js
+++ app.js
@@ -3,11 +3,24 @@ const path = require('path');
 
 const app = express();
 
+//template engine
+app.set('view engine', 'ejs');
+
 //middleware
 app.use(express.static('public'));
 
+//routes
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  //res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');
 });
 
 const port = 3000;
