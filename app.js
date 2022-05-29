c69ed20b4c4e6d24cdcfc9965807e8042e0bcd95
Nisa <n.nurb@hotmail.com>
2022-05-29 14:56:38 +0300

    photos model created, connected to db, create photo logic is implemented and fix some issue

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const ejs = require('ejs');
 const path = require('path');
 
+const Photo = require('./models/Photo');
+
 const app = express();
+
+//connect DB
+mongoose.connect('mongodb://localhost/pcat-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //template engine
 app.set('view engine', 'ejs');
 
 //middleware
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 //url'deki datayı okumamızı sağlar
 app.use(express.json());
 //url'deki datayı json fromatına dönüştürmemizi sağlar
 
 //routes
-app.get('/', (req, res) => {
-  //res.sendFile(path.resolve(__dirname, 'temp/index.html'));
-  res.render('index');
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({});
+  res.render('index', {
+    photos,
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -27,10 +40,10 @@ app.get('/add', (req, res) => {
   res.render('add');
 });
 
-app.post('/photos', (req,res) => {
-  console.log(req.body);
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
   res.redirect('/');
-})
+});
 
 const port = 3000;
 app.listen(port, () => {
