455ee480f46ad4b527572003627c4514d2090d5f
Nisa <n.nurb@hotmail.com>
2022-06-06 21:47:43 +0300

    upload photo

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
 
 const ejs = require('ejs');
 const path = require('path');
+const fs = require('fs');
 
 const Photo = require('./models/Photo');
 
@@ -19,14 +21,20 @@ app.set('view engine', 'ejs');
 
 //middleware
 app.use(express.static('public'));
+
 app.use(express.urlencoded({ extended: true }));
 //url'deki datayı okumamızı sağlar
+
 app.use(express.json());
 //url'deki datayı json fromatına dönüştürmemizi sağlar
 
+app.use(fileUpload());
+
 //routes
 app.get('/', async (req, res) => {
-  const photos = await Photo.find({});
+  //it'll sort the posts from new to old
+  const photos = await Photo.find({}).sort('-createdAt');
+
   res.render('index', {
     photos,
   });
@@ -37,8 +45,8 @@ app.get('/photos/:id', async (req, res) => {
   //res.render('about');
   const photo = await Photo.findById(req.params.id);
   res.render('photo', {
-    photo
-  })
+    photo,
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -50,8 +58,31 @@ app.get('/add', (req, res) => {
 });
 
 app.post('/photos', async (req, res) => {
-  await Photo.create(req.body);
-  res.redirect('/');
+
+  
+  const uploadDir = 'public/uploads';
+  //if the file (that we want to store our uploaded image) doesn't exist create one
+  if(!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  //the image that we uploaded
+  let uploadImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads/' + uploadImage.name;
+
+  //move the uploaded images to the file that we want
+  uploadImage.mv(
+    uploadPath,
+
+    //send the image to the db
+    async () => {
+      await Photo.create({
+        ...req.body,
+        image: '/uploads/' + uploadImage.name,
+      });
+      res.redirect('/');
+    }
+  );
 });
 
 const port = 3000;
